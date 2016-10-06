8b1d61d5f08da80fd8db08f34fc2979180c786bf
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2016-10-06 18:59:43 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,4 @@
+# Download and import data
 download.file(
     "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
     "exdata%2Fdata%2Fhousehold_power_consumption.zip")
@@ -13,6 +14,36 @@ my_data <-
         stringsAsFactors = F
     )[66637:69516, ]
 
+# Generate plot
+plot(
+    x = strptime(paste(my_data$Date, my_data$Time), "%d/%m/%Y %H:%M:%S"),
+    y = my_data$Sub_metering_1,
+    type = "n",
+    xlab = "",
+    ylab = "Energy sub metering"
+)
+lines(
+    x = strptime(paste(my_data$Date, my_data$Time), "%d/%m/%Y %H:%M:%S"),
+    y = my_data$Sub_metering_1,
+    col = "black"
+)
+lines(
+    x = strptime(paste(my_data$Date, my_data$Time), "%d/%m/%Y %H:%M:%S"),
+    y = my_data$Sub_metering_2,
+    col = "red"
+)
+lines(
+    x = strptime(paste(my_data$Date, my_data$Time), "%d/%m/%Y %H:%M:%S"),
+    y = my_data$Sub_metering_3,
+    col = "blue"
+)
+legend(
+    x = "topright",
+    legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+    lty = 1, col = c("black", "red", "blue")
+)
+
+# Export to png
 png(filename = "plot3.png", 480, 480)
 plot(
     x = strptime(paste(my_data$Date, my_data$Time), "%d/%m/%Y %H:%M:%S"),
